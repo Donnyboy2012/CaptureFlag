49dbfc213ff7e762a33bd652894cdecc2cc61a53
donovan roberts <donovan.roberts@wsu.edu>
2018-12-25 02:30:52 -0800

    Initialization

---
+++ src/captureflag/CaptureFlag.java
@@ -0,0 +1,10 @@
+package captureflag;
+
+public class CaptureFlag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SUCK A DICK");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author don
+ *
+ */
+module CaptureFlag {
+}
